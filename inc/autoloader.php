8bb312dfe80ac3e8642bb8daa4b6f6d32eb570a1
xlogion <xlogion@gmail.com>
2015-02-04 17:17:24 +0800

    Revert "初版本建立"
    This reverts commit 70dc858c31c3f21e5676d1145d955de3813d0db2.

--- inc/autoloader.php
+++
@@ -1,7 +0,0 @@
-<?php
-function xlogion_autoload ($ClassName) {
-	$in_path=str_replace('inc','',INC_DIR)."class/".$ClassName.".php";
-	$in_path=str_replace('\\',DIRECTORY_SEPARATOR,$in_path);
-	include($in_path);
-}
-spl_autoload_register("xlogion_autoload");
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("content-type:text/html;charset=utf-8");
-date_default_timezone_set('Asia/Shanghai');
-include("inc/config.php");
-$db = new xlogion\pdo_class($db_config);
-//详细见readme
-$db->close();
